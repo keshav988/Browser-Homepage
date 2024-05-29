43c39b9af3da3d294a7d03ce0306b4161bbb88b9
Jayanta Ojha <jayantaojha686@gmail.com>
2024-05-29 13:10:46 +0530

    Weather app done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const greeting = document.querySelector('#greeting');
-
+const weather = document.querySelector('#weather');
 
 
 
@@ -10,7 +10,7 @@ function showGreeting() {
 
     if (hours >= 5 && hours < 8) {
         greeting.innerHTML = "Rise and shine! JAYANTA";
-    } 
+    }
     else if (hours >= 8 && hours < 12) {
         greeting.innerHTML = "Good morning, JAYANTA";
     }
@@ -29,9 +29,33 @@ function showGreeting() {
 
 }
 
-
-// Update the greeting initially
 showGreeting();
 
-// Set interval to update the greeting every minute (60000 milliseconds)
-setInterval(showGreeting, 60000);
+setInterval(showGreeting, 60000); // every minuets
+
+
+// getWeather function
+async function getWeather() {
+    const latitude = 26.71;
+    const longitude = 88.4285;
+
+    const WEATHER_URL = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
+
+    try {
+        let response = await fetch(WEATHER_URL);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        let data = await response.json();
+        weather.innerHTML = `${data.current_weather.temperature}° Siliguri`
+
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
+getWeather();
+
+setInterval(getWeather, 60000); // every minutes
+
